76169d82ffa31c45ec4e68306f7641ff82da9803
JamesBarnsley <jamesbarnsleyfreelance@googlemail.com>
2024-10-29 14:40:03 +0000

    Config fix

--- connector/config.php
+++ connector/config.php
@@ -4,7 +4,7 @@ return [
     'sources' => [
         'default' => [
             'root' => './../../../../public/editor-upload/',
-            'baseurl' => 'http://127.0.0.1:8000/editor-upload/',
+            'baseurl' => '/editor-upload/',
             'createThumb' => true,
             'thumbFolderName' => '_thumbs',
             'extensions' => ['jpg', 'png', 'gif', 'jpeg']
